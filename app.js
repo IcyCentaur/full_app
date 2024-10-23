b7bd25dad46c49ca4ad050cb416ae5742c3c0d2a
Darsh <darshrawat03@gmail.com>
2024-10-23 23:11:55 +0530

    initial-commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello, this is my simple Node.js webpage!</h1>");
+  res.send("<h1>Hello, this is my simple Node.js pls work man webpage!</h1>");
 });
 
 app.listen(port, () => {
